72f6b39e5db05e196ae0e9cded7b3857ca9295a5
romain s <romain.schoenberger@gmail.com>
2020-01-27 16:44:05 +0100

    // SOME BASIS

---
+++ BATHSALT/Assets/Scenes/SCRIPTS/Attributes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributes
+{
+    public Attributes()
+    {
+        HP          = 0f;
+        ENERGY      = 0f;
+        EXPERIENCE  = 0f;
+
+        computeStats();
+    }
+
+    // -----------------------------
+    // MAIN ATTRIBUTES
+    // -----------------------------
+    public float HP;
+    public float ENERGY;
+    public float EXPERIENCE;
+
+
+    // -----------------------------
+    // COMPUTED ATTRIBUTES
+    // -----------------------------
+    public int LEVEL;
+
+    public void computeStats()
+    {
+        LEVEL = EXPERIENCE / 100 ;
+    }
+
+}
